bd9b5dfb82b63dfdb166b8983c486365c123efe1
rodriguezefs <rodriguezefs@live.com>
2019-04-06 07:51:02 -0400

    GetTweetHome

---
+++ erTw/Extentions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace erTw
+{
+    public static class Extensions
+    {
+        public static string ToWebString(this SortedDictionary<string, string> source)
+        {
+            var body = new StringBuilder();
+
+            foreach (var requestParameter in source)
+            {
+                body.Append(requestParameter.Key);
+                body.Append("=");
+                body.Append(Uri.EscapeDataString(requestParameter.Value));
+                body.Append("&");
+            }
+            //remove trailing '&'
+            body.Remove(body.Length - 1, 1);
+
+            return body.ToString();
+        }
+    }
+}
---
+++ erTw/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace erTw {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        const string ConsumerKey = "cpPeZ6vPXUPbwbiAKrvBvw";
+        const string ConsumerKeySecret = "X6jWAvQPpXyfJaznRtR67mGK6AQLkp3HSMpzf1AFY";
+        const string AccessToken = "51357501-ZlzXYYQNJwBrtO1mWmQGfdZxvt6OnxzVEg2g564nI";
+        const string AccessTokenSecret = "fhb1CAkVSZuoeb5jjWuEWQLxYZACSUeQ1r7fPD0";
+
+
+        public MainWindow() {
+            InitializeComponent();
+
+            GetTweets();
+        }
+
+        private void GetTweets() {
+
+            var twitter = new Twitter(ConsumerKey, ConsumerKeySecret, AccessToken, AccessTokenSecret);
+
+            var response = twitter.GetTweetsHome("rodriguezefs", 100);
+
+            dynamic timeline = JsonConvert.DeserializeObject(response);
+            lstTweets.DataContext = timeline;
+        }
+    }
+    
+}
