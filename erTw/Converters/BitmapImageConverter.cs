e5df092bc6fe5af68c8e7d6531d6764afce425b8
rodriguezefs <rodriguezefs@live.com>
2019-05-07 09:05:39 -0400

    url to image (not working)

---
+++ erTw/Converters/BitmapImageConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace erTw {
+    public class BitmapImageConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            BitmapImage bi = new BitmapImage();
+
+            bi.BeginInit();
+            bi.UriSource = new Uri((string)value, UriKind.RelativeOrAbsolute);
+            bi.EndInit();
+
+            return bi;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
