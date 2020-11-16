d045f6435c6dab422e84418793ece51f3b722b51
rodriguezefs <rodriguezefs@live.com>
2020-11-15 20:46:53 -0400

    erTw.Blazor

---
+++ Twitter.Lib/TimeLine.cs
@@ -0,0 +1,12 @@
+﻿using erTw.Twitter.Lib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Twitter.Lib
+{
+    public class TimeLine
+    {
+        public List<Tweet> Tweets { get; set; }
+    }
+}
---
+++ erTw.Blazor/Pages/Index.razor.cs
@@ -0,0 +1,30 @@
+﻿using erTw.Twitter.Lib;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Humanizer;
+
+namespace erTw.Blazor.Pages
+{
+    public partial class Index: ComponentBase
+    {
+        const string ConsumerKey = "cpPeZ6vPXUPbwbiAKrvBvw";
+        const string ConsumerKeySecret = "X6jWAvQPpXyfJaznRtR67mGK6AQLkp3HSMpzf1AFY";
+        const string AccessToken = "51357501-ZlzXYYQNJwBrtO1mWmQGfdZxvt6OnxzVEg2g564nI";
+        const string AccessTokenSecret = "fhb1CAkVSZuoeb5jjWuEWQLxYZACSUeQ1r7fPD0";
+
+        public dynamic TimeLine { get; set; }
+
+        protected override void OnInitialized()
+        { 
+            var twitter = new Twitter.Lib.Twitter(ConsumerKey, ConsumerKeySecret, AccessToken, AccessTokenSecret);
+            var response = twitter.GetTweetsHome("rodriguezefs", 100);
+            TimeLine = JsonConvert.DeserializeObject(response);
+
+        }
+    }
+}
